912f480161b11de01952e873bf227ce5e48e6fed
Ekaterina <Kklyushnikova@bk.ru>
2021-12-16 16:20:03 +0300

    New project

--- ConsoleAppTE/Program.cs
+++ ConsoleAppTE/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppTE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
